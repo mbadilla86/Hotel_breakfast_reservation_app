671b85a4bb0757f551a563eaf0288a9ce608e5ba
Mauricio Badilla <mbadilla86@gmail.com>
2021-05-05 12:55:21 -0600

    DUS010 add migration CreateJoinTableUserReservation user reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_184753) do
+ActiveRecord::Schema.define(version: 2021_05_05_185304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2021_05_05_184753) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_reservations_on_room_id"
+  end
+
+  create_table "reservations_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "reservation_id", null: false
+    t.index ["reservation_id", "user_id"], name: "index_reservations_users_on_reservation_id_and_user_id"
+    t.index ["user_id", "reservation_id"], name: "index_reservations_users_on_user_id_and_reservation_id"
   end
 
   create_table "rooms", force: :cascade do |t|
