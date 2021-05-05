5a3f2f6b5d6b4ab54bebaa3818c76713386a5980
Mauricio Badilla <mbadilla86@gmail.com>
2021-05-05 15:14:26 -0600

    DUS014 add controller user devise, se add field y stong params

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :reservations
   resources :rooms
   resources :hotels
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+        registrations: 'users/registrations'
+
+      }
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
