0718ce9b56db9d8ab0ea15140df3bf1172866993
Mauricio Badilla <mbadilla86@gmail.com>
2021-05-05 12:36:09 -0600

    DUS008 add devise admin, first_name, last_name, hotel:references.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_182426) do
+ActiveRecord::Schema.define(version: 2021_05_05_183235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "hotel_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["hotel_id"], name: "index_admins_on_hotel_id"
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "breakfasts", force: :cascade do |t|
     t.bigint "reservation_id", null: false
@@ -62,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_182426) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "admins", "hotels"
   add_foreign_key "breakfasts", "reservations"
   add_foreign_key "reservations", "rooms"
   add_foreign_key "rooms", "hotels"
