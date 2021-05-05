f11f2705534ee85f7e97b936fa9433066a361f79
Mauricio Badilla <mbadilla86@gmail.com>
2021-05-05 11:42:57 -0600

    DUS006 add devise, install

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :reservations
   resources :rooms
   resources :hotels
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
