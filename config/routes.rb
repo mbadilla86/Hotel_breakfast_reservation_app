55aac4771b1480cac722f9a55880bdfb505c229b
Mauricio Badilla <mbadilla86@gmail.com>
2021-05-05 20:38:24 -0600

    UUS003 add routes resources :user_reservations, ademas se modifico las rutas para admin y user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
-  resources :user_breakfasts
-  resources :hotels
-  resources :rooms
-  resources :breakfasts
-  resources :reservations
-  get 'home/index'
   
-  devise_for :admins
+  #get 'home/index'
+  authenticate :user do
+    resources :user_reservations
+    resources :user_breakfasts
+
+  end
+  
+  authenticate :admin do
+    resources :hotels
+    resources :rooms
+    resources :reservations
+    resources :breakfasts
+    
+  end
   devise_for :users, controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
+  devise_for :admins
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
