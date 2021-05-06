7114d545b63b0cea767435d1517e7c74437f2629
Mauricio Badilla <mbadilla86@gmail.com>
2021-05-06 05:43:11 -0600

    DUS017 add admin controller y sus rutas

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations'
+  }
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
